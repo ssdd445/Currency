fa6c1cdf699cf59f3b675a5eee63b895b2ce6776
Saud Waqar <saudwaqarmalik@gmail.com>
2024-02-04 21:00:32 +0500

    Basic UI creation done. Landscape and portrait support done. Buttons and their listeners are bind in RxSwift

--- Currency/Currency/Scenes/CurrencyConversion/View/CurrencyConversionViewController.swift
+++ Currency/Currency/Scenes/CurrencyConversion/View/CurrencyConversionViewController.swift
@@ -9,6 +9,14 @@ class CurrencyConversionViewController: UIViewController {
         loadingView.center = view.center
         return loadingView
     }()
+    
+    @IBOutlet weak var buttonFrom: UIButton!
+    @IBOutlet weak var buttonTo: UIButton!
+    @IBOutlet weak var buttonSwap: UIButton!
+    @IBOutlet weak var buttonDetail: UIButton!
+    
+    @IBOutlet weak var txtFieldFrom: CleanTextField!
+    @IBOutlet weak var txtFieldTo: CleanTextField!
 
     private let disposeBag = DisposeBag()
     
@@ -30,5 +38,45 @@ class CurrencyConversionViewController: UIViewController {
         viewModel.isLoading.subscribe({ event in
             self.view.isUserInteractionEnabled = !(event.element ?? false)
         }).disposed(by: disposeBag)
+        
+        buttonFrom.rx.tap
+            .bind(to: viewModel.buttonFromTap)
+            .disposed(by: disposeBag)
+        
+        buttonTo.rx.tap
+            .bind(to: viewModel.buttonToTap)
+            .disposed(by: disposeBag)
+        
+        buttonSwap.rx.tap
+            .bind(to: viewModel.buttonSwapTap)
+            .disposed(by: disposeBag)
+        
+        buttonDetail.rx.tap
+            .bind(to: viewModel.buttonDetailsTap)
+            .disposed(by: disposeBag)
+        
+        viewModel.fromTextField
+            .bind(to: txtFieldFrom.rx.text)
+            .disposed(by: disposeBag)
+        
+        txtFieldFrom.rx.text
+            .orEmpty
+            .map { String($0.prefix(Constants.textFieldLength)) }
+            .bind(to: viewModel.fromTextField)
+            .disposed(by: disposeBag)
+
+        txtFieldTo.rx.text
+            .orEmpty
+            .map { String($0.prefix(Constants.textFieldLength)) }
+            .bind(to: viewModel.toTextField)
+            .disposed(by: disposeBag)
+
+        viewModel.fromOutputSubject
+            .bind(to: txtFieldTo.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.toOutputSubject
+            .bind(to: txtFieldFrom.rx.text)
+            .disposed(by: disposeBag)
     }
 }
--- Currency/Currency/Scenes/CurrencyConversion/ViewModel/CurrencyConversionViewModel.swift
+++ Currency/Currency/Scenes/CurrencyConversion/ViewModel/CurrencyConversionViewModel.swift
@@ -3,16 +3,84 @@ import RxCocoa
 import Foundation
 
 class CurrencyConversionViewModel {
-    let buttonTap = PublishSubject<Void>()
+    let buttonFromTap = PublishSubject<Void>()
+    let buttonToTap = PublishSubject<Void>()
+    let buttonDetailsTap = PublishSubject<Void>()
+    let buttonSwapTap = PublishSubject<Void>()
+    
+    let fromTextField = PublishSubject<String>()
+    let toTextField = PublishSubject<String>()
+    
+//    let subject = BehaviorSubject<Int>(value: 10)
+    let fromOutputSubject = PublishSubject<String>()
+    let toOutputSubject = PublishSubject<String>()
+
+//    let fromOutputSubject = PublishSubject<String>()
+//    let toOutputSubject = PublishSubject<String>()
+    
+    private let disposeBag = DisposeBag()
     let isLoading = PublishSubject<Bool>()
     
     init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.fromTextField.onNext("1")
+        }
         setupBindings()
     }
-
-    private func setupBindings() {
-
+    
+    func temp() {
+        self.isLoading.onNext(true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.isLoading.onNext(false)
+        }
     }
+    
+    private func setupBindings() {
+        buttonFromTap.subscribe(onNext: { [weak self] in
+            print("\(#line)")
+            self?.temp()
+        }).disposed(by: disposeBag)
+        
+        buttonToTap.subscribe(onNext: { [weak self] in
+            print("\(#line)")
+            self?.temp()
+        }).disposed(by: disposeBag)
+        
+        buttonDetailsTap.subscribe(onNext: { [weak self] in
+            print("\(#line)")
+            self?.temp()
+        }).disposed(by: disposeBag)
+        
+        buttonSwapTap.subscribe(onNext: { [weak self] in
+            print("\(#line)")
+            self?.temp()
+        }).disposed(by: disposeBag)
 
-    private let disposeBag = DisposeBag()
+        fromTextField
+            .compactMap { Int($0) ?? 1}
+            .map { $0 * 2 }
+            .map { "\($0)" }
+            .bind(to: fromOutputSubject)
+            .disposed(by: disposeBag)
+        
+        toTextField
+            .compactMap { Int($0) ?? 1 }
+            .map { $0 * 2 }
+            .map { "\($0)" }
+            .bind(to: toOutputSubject)
+            .disposed(by: disposeBag)
+
+
+        fromOutputSubject
+            .subscribe(onNext: { result in
+                print("From Result: \(result)")
+            })
+            .disposed(by: disposeBag)
+        
+        toOutputSubject
+            .subscribe(onNext: { result in
+                print("To Result1: \(result)")
+            })
+            .disposed(by: disposeBag)
+    }
 }
